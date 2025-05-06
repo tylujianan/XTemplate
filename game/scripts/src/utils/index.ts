bd8749546d2e36716452b58686a4c8f5f97e7719
tylujianan <ljn199379@icloud.com>
2025-05-06 13:04:26 +0800

    Initial commit

---
+++ game/scripts/src/utils/index.ts
@@ -0,0 +1,12 @@
+// 引入所有的lua模块
+require('aeslua');
+require('decrypt');
+require('json');
+require('md5');
+require('popups');
+if (IsServer()) {
+    require('timers');
+}
+
+// rename SHA and make it global
+globalThis.SHA = require('sha');
